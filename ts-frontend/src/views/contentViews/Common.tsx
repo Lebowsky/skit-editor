82111470d35d36f13f1de8764dbaf94a417a06ac
And <ase@crimea.com>
2024-05-28 17:52:41 +0300

    remove log

--- ts-frontend/src/views/contentViews/Common.tsx
+++ ts-frontend/src/views/contentViews/Common.tsx
@@ -6,7 +6,6 @@ import { ParamsFields } from "../operationViews/OperationElements/ParamsHandlers
 export default function Common() {
   const { currentContextType, currentContent } = useSimpleUI() as IContextProviderData
   if (currentContextType !== contextTypes.common) return null
-  console.log(currentContent)
   const fields: ParamsFields[] = [
     {name: 'ConfigurationName', type: 'text', title: 'Name'},
     {name: 'ConfigurationDescription', type: 'text', title: 'Description'},
